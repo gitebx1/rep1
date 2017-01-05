67fcb186dc173716cdbb55be22a1ccf74659aba2
venkatgn <venkatadrin.g@gmail.com>
2017-01-05 13:14:44 +0530

    dfd

---
+++ myproj/src/test/java/myproj/Evenodd.java
@@ -0,0 +1,16 @@
+package myproj;
+
+public class Evenodd 
+{
+
+	public static void main(String[] args)
+	{
+		int n=8;
+		if(n%2==0)
+			System.out.println("it is even");
+		else
+			System.out.println("it is odd");
+
+	}
+
+}
